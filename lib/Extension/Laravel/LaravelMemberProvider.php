71a980ebe744b466a5086c296e19073e2eccc4b7
Ariel D'Alessandro <dalessandro.ariel@gmail.com>
2024-10-03 18:39:41 +0200

    add basic support for model fiels

--- lib/Extension/Laravel/LaravelMemberProvider.php
+++ lib/Extension/Laravel/LaravelMemberProvider.php
@@ -2,17 +2,28 @@
 
 namespace Phpactor\Extension\Laravel;
 
+use Phpactor\Extension\Laravel\Providers\ModelFieldsProvider;
+use Phpactor\WorseReflection\Core\Deprecation;
+use Phpactor\WorseReflection\Core\Inference\Frame;
 use Phpactor\WorseReflection\Core\Reflection\Collection\ChainReflectionMemberCollection;
+use Phpactor\WorseReflection\Core\Reflection\Collection\HomogeneousReflectionMemberCollection;
 use Phpactor\WorseReflection\Core\Reflection\Collection\ReflectionMemberCollection;
 use Phpactor\WorseReflection\Core\Reflection\ReflectionClassLike;
 use Phpactor\WorseReflection\Core\ServiceLocator;
 use Phpactor\WorseReflection\Core\TypeFactory;
 use Phpactor\WorseReflection\Core\Virtual\ReflectionMemberProvider;
+use Phpactor\WorseReflection\Core\Virtual\VirtualReflectionProperty;
+use Phpactor\WorseReflection\Core\Visibility;
 
 class LaravelMemberProvider implements ReflectionMemberProvider
 {
     private const ELOQUENT_MODEL = '\\Illuminate\\Database\\Eloquent\\Model';
     private const ELOQUENT_BUILDER = '\\Illuminate\\Database\\Eloquent\\Builder';
+
+    public function __construct(
+        private readonly ModelFieldsProvider $modelFieldsProvider,
+    ) {
+    }
 
     public function provideMembers(ServiceLocator $locator, ReflectionClassLike $class): ReflectionMemberCollection
     {
@@ -24,6 +35,32 @@ class LaravelMemberProvider implements ReflectionMemberProvider
             $builder = $locator->reflector()->reflectClass(self::ELOQUENT_BUILDER);
 
             $members[] = $builder->type()->members()->methods();
+
+            // here could add the virtual methods for the specifc class
+            if ($class->type()->name()->head() != "Illuminate") {
+                $properties = [];
+                try {
+                    foreach ($this->modelFieldsProvider->get($class->type()->name()->full()) as $field) {
+                        $properties[] = new VirtualReflectionProperty(
+                            $class->position(),
+                            $class,
+                            $class,
+                            $field['name'],
+                            new Frame(),
+                            $class->docblock(),
+                            $class->scope(),
+                            Visibility::public(),
+                            $field['type'],
+                            $field['type'],
+                            new Deprecation(false),
+                        );
+                    }
+
+                    $memberCollection = HomogeneousReflectionMemberCollection::fromMembers($properties);
+                    $members[] = $memberCollection;
+                } catch (\Throwable) {
+                }
+            }
         }
 
         return ChainReflectionMemberCollection::fromCollections($members);
---
+++ lib/Extension/Laravel/Providers/ModelFieldsProvider.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Phpactor\Extension\Laravel\Providers;
+
+use Phpactor\Extension\Laravel\ArtisanRunner;
+use Phpactor\WorseReflection\Core\TypeFactory;
+
+class ModelFieldsProvider
+{
+    public function __construct(
+        private readonly ArtisanRunner $artisanRunner,
+    ) {
+    }
+
+    public function get(string $model): array
+    {
+        $info = $this->artisanRunner->run('model:show', [$model, '--json']);
+
+        return array_map(
+            fn ($field) => [
+                'name' => $field['name'],
+                'type' => TypeFactory::fromString($field['type'] ?? ''),
+            ],
+            $info['attributes'],
+        );
+    }
+}

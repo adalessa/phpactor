c79cbe77975fff7625294ccbc07b55f8210e3083
Ariel D'Alessandro <dalessandro.ariel@gmail.com>
2024-10-04 14:29:03 +0200

    add cache

--- lib/Extension/Laravel/Providers/RoutesProvider.php
+++ lib/Extension/Laravel/Providers/RoutesProvider.php
@@ -3,31 +3,35 @@
 namespace Phpactor\Extension\Laravel\Providers;
 
 use Phpactor\Extension\Laravel\ArtisanRunner;
+use Phpactor\WorseReflection\Core\Cache;
 use Symfony\Component\Process\Process;
 
 class RoutesProvider
 {
     public function __construct(
         private readonly ArtisanRunner $artisanRunner,
+        private readonly Cache $cache,
     ) {
     }
 
     public function get(): array
     {
-        $routes = $this->artisanRunner->run('routes:list', ['--json']);
+        return $this->cache->getOrSet('laravel-routes', function(): array {
+            $routes = $this->artisanRunner->run('routes:list', ['--json']);
 
-        $process = new Process(['php', 'artisan', 'route:list', '--json']);
-        $process->run();
-        if (!$process->isSuccessful()) {
-            return [];
-        }
-        $routes = json_decode($process->getOutput(), true);
+            $process = new Process(['php', 'artisan', 'route:list', '--json']);
+            $process->run();
+            if (!$process->isSuccessful()) {
+                return [];
+            }
+            $routes = json_decode($process->getOutput(), true);
 
-        return array_map(function ($route) {
-            return [
-                'name' => $route['name'],
-                'path' => $route['uri'],
-            ];
-        }, $routes);
+            return array_map(function ($route) {
+                return [
+                    'name' => $route['name'],
+                    'path' => $route['uri'],
+                ];
+            }, $routes);
+        });
     }
 }

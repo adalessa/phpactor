bb32dc2e218b80216e6cb92726deb29414803cbd
Ariel D'Alessandro <dalessandro.ariel@gmail.com>
2024-10-04 14:29:03 +0200

    add cache

--- lib/Extension/Laravel/Providers/ConfigsProvider.php
+++ lib/Extension/Laravel/Providers/ConfigsProvider.php
@@ -3,22 +3,26 @@
 namespace Phpactor\Extension\Laravel\Providers;
 
 use Phpactor\Extension\Laravel\ArtisanRunner;
+use Phpactor\WorseReflection\Core\Cache;
 
 class ConfigsProvider
 {
     public function __construct(
         private readonly ArtisanRunner $artisanRunner,
+        private readonly Cache $cache,
     ) {
     }
 
     public function get(): array
     {
-        return $this->artisanRunner->run(
-            'tinker',
-            [
-                '--execute',
-                'echo json_encode(collect(\Illuminate\Support\Arr::dot(\Illuminate\Support\Facades\Config::all()))->keys()->filter(fn($key) => is_string($key))->values()->all());'
-            ]
-        );
+        return $this->cache->getOrSet('laravel-configs', function(): array {
+            return $this->artisanRunner->run(
+                'tinker',
+                [
+                    '--execute',
+                    'echo json_encode(collect(\Illuminate\Support\Arr::dot(\Illuminate\Support\Facades\Config::all()))->keys()->filter(fn($key) => is_string($key))->values()->all());'
+                ]
+            );
+        });
     }
 }

0d8e2e5ffbe0c64e48922d2e044d64ca9a3ce061
Ariel D'Alessandro <dalessandro.ariel@gmail.com>
2024-10-05 11:49:06 +0200

    improve relations

--- lib/Extension/Laravel/Providers/ModelFieldsProvider.php
+++ lib/Extension/Laravel/Providers/ModelFieldsProvider.php
@@ -3,8 +3,10 @@
 namespace Phpactor\Extension\Laravel\Providers;
 
 use Phpactor\Extension\Laravel\ArtisanRunner;
+use Phpactor\WorseReflection\Core\Type;
 use Phpactor\WorseReflection\Core\Cache;
 use Phpactor\WorseReflection\Core\TypeFactory;
+use Phpactor\WorseReflection\Reflector;
 
 class ModelFieldsProvider
 {
@@ -14,22 +16,69 @@ class ModelFieldsProvider
     ) {
     }
 
-    public function get(string $model): array
+    public function get(string $model, Reflector $reflector): array
     {
-        return $this->cache->getOrSet('laravel-model-'.$model, function() use ($model): array {
+        return $this->cache->getOrSet('laravel-model-'.$model, function() use ($model, $reflector): array {
             $info = $this->artisanRunner->run('model:show', [$model, '--json']);
 
             if (null == $info) {
                 return [];
             }
 
-            return array_map(
-                fn ($field) => [
-                    'name' => $field['name'],
-                    'type' => TypeFactory::fromString($field['type'] ?? ''),
-                ],
-                $info['attributes'],
+            return array_merge(
+                 array_map(
+                    fn ($field) => [
+                        'name' => $field['name'],
+                        'type' => $this->createType($field),
+                    ],
+                    $info['attributes'] ?? [],
+                ),
+                 array_map(
+                    fn ($relation) => [
+                        'name' => $relation['name'],
+                        'type' => $this->createTypeFromRelation($relation, $reflector),
+                    ],
+                    $info['relations'] ?? [],
+                )
             );
         });
+    }
+
+    private function createType(array $field): Type
+    {
+        $type = match($field['type']) {
+            'varchar', 'text' => TypeFactory::string(),
+            'integer' => TypeFactory::int(),
+            default => TypeFactory::string(),
+        };
+
+        // TODO: suuport more default casts https://laravel.com/docs/11.x/eloquent-mutators#attribute-casting
+        $type = match ($field['cast']) {
+            'datetime', 'date' => TypeFactory::class('\Illuminate\Support\Carbon'),
+            'bool' => TypeFactory::bool(),
+            'attribute' => TypeFactory::string(),
+            'int' => TypeFactory::int(),
+            default => $type,
+        };
+
+        // TODO: need to handle cast custom casts.
+
+        // TODO: handle the relationships
+
+        if (isset($field['nullable']) && $field['nullable']) {
+            $type = TypeFactory::nullable($type);
+        }
+
+        return $type;
+    }
+
+    private function createTypeFromRelation(array $relation, Reflector $reflector): Type
+    {
+        $type = TypeFactory::class($relation['related']);
+
+        return match ($relation['type']) {
+            'BelongsToMany', 'HasMany' => TypeFactory::collection($reflector, '\Illuminate\Database\Eloquent\Collection', $relation['related']),
+            default => $type,
+        };
     }
 }
